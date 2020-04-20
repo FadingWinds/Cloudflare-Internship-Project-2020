61a591a1597d43357b877add2a6f04dd14e264c5
FadingWinds <wq.tang26@gmail.com>
2020-04-20 12:57:33 -0400

    Final adjustments

--- index.js
+++ index.js
@@ -1,34 +1,41 @@
 // Define url to request
 const target_url = 'https://cfw-takehome.developers.workers.dev/api/variants';
 
-// Requirement 1: Request the URLs from the API
-
 addEventListener('fetch', async event => {
   event.respondWith(handleRequest(target_url));
 });
 
-/*
-async function handleRequest(request) {
-  //const cachedResponse = await caches.match(request);
-  // Define init of the fetch method
-  const init = {
-	method: 'GET',
-	headers: {
-	  'content-type': 'application/json;charset=UTF-8',
-	},
-  };
-  const get_response = await fetch(request,init);
-  const results = await get_response.json(); // Get JSON format results
-  const body_json = JSON.stringify(results); // Turning results to an array
-  var urls = JSON.parse(body_json);
-  console.log(urls);
-  const bi_rand = Math.round(Math.random());
-  return new Response(body_json, init);
-};
-*/
+// Define a series of rewriter handlers
+class TITLErewriter {
+	element(element) {
+		element.setInnerContent("Cloudflare Internship Application Project - Wenqing Tang");
+	}
+}
+class HEADrewriter {
+	element(element) {
+		element.prepend("Welcome to the page of");
+	}
+}
+class TEXTrewriter {
+	element(element) {
+		element.setInnerContent(
+			"Thanks for reviewing my project! "
+			);
+		element.append("Hope you stay healthy and safe :) ");
+		element.append(
+			"Click the following button to visit my (in-development) personal website based on Hexo."
+			);
+	}
+}
+class LINKrewriter {
+	element(element) {
+		element.setAttribute("href", 'https://fadingwinds.me/');
+		element.setInnerContent("Click and Go visit!")
+	}
+}
+
 
 async function handleRequest(request) {
-  //const cachedResponse = await caches.match(request);
   // Define init of the fetch method
   const init = {
 	method: 'GET',
@@ -36,14 +43,28 @@ async function handleRequest(request) {
 	  'content-type': 'application/json;charset=UTF-8',
 	},
   };
-  const get_response = await fetch(request,init);
-  const results = await get_response.json(); // Get JSON format results
-  const body_json = JSON.stringify(results); // Turning results to an array
-  var urls = JSON.parse(body_json);
-  console.log(urls);
+  const resp = await fetch(request,init);
+  // Get JSON format results
+  const results = await resp.json(); 
+  console.log("Fetched JSON: " + results);
+  // Turning results to an array 
+  const body_json = JSON.stringify(results);
+  const urls = JSON.parse(body_json);
+  // Setup randomized visit
   const bi_rand = Math.round(Math.random());
-  console.log(bi_rand);
-  const url_resp = await fetch(urls.variants[bi_rand]);
-  return url_resp;
-//  return new Response(body_json, init);
+  console.log("Acquired Binary Random Int:" + bi_rand);
+  // Setup cookie for different page; expires in 3 days
+  const cookie_init = {
+  	method: 'GET',
+  	headers: {
+  		'Set-Cookie': 'id=ckforpage+bi_rand.toString(); Max-Age = 259200; Secure'
+  	},
+  };
+  const url_resp = await fetch(urls.variants[bi_rand], cookie_init);
+  const rewriter = new HTMLRewriter()
+  	.on('title', new TITLErewriter())
+  	.on('h1', new HEADrewriter())
+  	.on('p', new TEXTrewriter())
+  	.on('a', new LINKrewriter());
+  return rewriter.transform(url_resp);
 };
