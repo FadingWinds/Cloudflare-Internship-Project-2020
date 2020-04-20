ec7c8b2bcad63155e7b551f9364971cc6ace4537
FadingWinds <wq.tang26@gmail.com>
2020-04-19 23:13:36 -0400

    Request 1 finished

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
+// Define url to request
+const target_url = 'https://cfw-takehome.developers.workers.dev/api/variants'
+
+// Define getting response
+const fetchInit = {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json;charset=UTF-8'
+  }
+};
+
 addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
+  //event.respondWith(handleRequest(event.request))
+  event.respondWith(handleRequest(target_url))
 })
 /**
  * Respond with hello worker text
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+  //const cachedResponse = await caches.match(request);
+  const get_response = await fetch(request,fetchInit);
+  return get_response;
 }
