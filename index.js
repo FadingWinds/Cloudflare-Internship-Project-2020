b16c1c622a1e032267817d123563bc40f47c4ff8
FadingWinds <wq.tang26@gmail.com>
2020-04-20 10:52:33 -0400

    Requirement 2&3 completed

--- index.js
+++ index.js
@@ -1,24 +1,49 @@
 // Define url to request
-const target_url = 'https://cfw-takehome.developers.workers.dev/api/variants'
+const target_url = 'https://cfw-takehome.developers.workers.dev/api/variants';
 
-// Define getting response
-const fetchInit = {
-  method: 'GET',
-  headers: {
-    'Content-Type': 'application/json;charset=UTF-8'
-  }
-};
+// Requirement 1: Request the URLs from the API
 
-addEventListener('fetch', event => {
-  //event.respondWith(handleRequest(event.request))
-  event.respondWith(handleRequest(target_url))
-})
-/**
- * Respond with hello worker text
- * @param {Request} request
- */
+addEventListener('fetch', async event => {
+  event.respondWith(handleRequest(target_url));
+});
+
+/*
 async function handleRequest(request) {
   //const cachedResponse = await caches.match(request);
-  const get_response = await fetch(request,fetchInit);
-  return get_response;
-}
+  // Define init of the fetch method
+  const init = {
+	method: 'GET',
+	headers: {
+	  'content-type': 'application/json;charset=UTF-8',
+	},
+  };
+  const get_response = await fetch(request,init);
+  const results = await get_response.json(); // Get JSON format results
+  const body_json = JSON.stringify(results); // Turning results to an array
+  var urls = JSON.parse(body_json);
+  console.log(urls);
+  const bi_rand = Math.round(Math.random());
+  return new Response(body_json, init);
+};
+*/
+
+async function handleRequest(request) {
+  //const cachedResponse = await caches.match(request);
+  // Define init of the fetch method
+  const init = {
+	method: 'GET',
+	headers: {
+	  'content-type': 'application/json;charset=UTF-8',
+	},
+  };
+  const get_response = await fetch(request,init);
+  const results = await get_response.json(); // Get JSON format results
+  const body_json = JSON.stringify(results); // Turning results to an array
+  var urls = JSON.parse(body_json);
+  console.log(urls);
+  const bi_rand = Math.round(Math.random());
+  console.log(bi_rand);
+  const url_resp = await fetch(urls.variants[bi_rand]);
+  return url_resp;
+//  return new Response(body_json, init);
+};
